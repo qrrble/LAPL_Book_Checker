14897c2a936fd62562db64a6d34f8ec1c8928d2b
Anson L <qrrble@users.noreply.github.com>
2020-02-19 21:27:06 -0800

    Library branch checkboxes

--- sl_results.py
+++ sl_results.py
@@ -23,15 +23,38 @@ if path.exists("bookinfo.csv"):
 		book_pd = book_pd.drop_duplicates()
 
 	sl.subheader('Branches')
-	is_central = sl.checkbox('Central Library')
-	if is_central:
-		book_pd = book_pd[book_pd['Library'].str.contains('Central Library')]
 
+	# Checkboxes for filter for specific library branches
+	is_central = sl.checkbox('Central Library',value = True)
+	is_littletokyo = sl.checkbox('Little Tokyo',value = True)
+	is_marvista = sl.checkbox('Mar Vista',value = True)
+	is_palms = sl.checkbox('Palms-Rancho Park',value = True)
+
+	branch_names = []
+
+	if is_central:
+		branch_names.append('Central Library')
+
+	if is_littletokyo:
+		branch_names.append('Little Tokyo Branch')
+
+	if is_marvista:
+		branch_names.append('Mar Vista Branch')
+
+	if is_palms:
+		branch_names.append('Palms-Rancho Park Branch')
+
+	# Filters out library branches
+	book_pd = book_pd[book_pd['Library'].str.contains('|'.join(branch_names))]
 
 	book_pd = book_pd.sort_values(by=['Library'])
 	
-
 	book_pd
+
+	if sl.button('Save filtered results to .csv'):
+		book_pd[['Title','Library','Call_Number']].to_csv('lookup.csv',index=False)
+		sl.write('Results saved to lookup.csv')
+
 
 else:
 	sl.write('ERROR: bookinfo.csv file not found!')
